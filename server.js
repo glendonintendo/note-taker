3d35f98bed34ac375035f51e71710dbf6e67d40a
glendo <gleneggers64@gmail.com>
2021-04-20 11:47:49 -0500

    add post request; add get request by id param; add helper functions for get and post requests

--- server.js
+++ server.js
@@ -1,12 +1,64 @@
 const express = require('express');
 const { notes } = require('./data/notes');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+function findById(id, notesArray) {
+    return notesArray.filter(note => note.id === id)[0];
+};
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    )
+
+    return note;
+};
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.body || typeof note.body !== 'string') {
+        return false;
+    }
+
+    return true;
+}
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
