65c28fb2c27659e924df9b838ad45c2ba5bb50bf
glendo <gleneggers64@gmail.com>
2021-04-21 10:44:49 -0500

    add api and html routes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { notes } = require('./data/notes');
-const fs = require('fs');
-const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,84 +11,8 @@ app.use(express.json());
 // serves files in public
 app.use(express.static('public'));
 
-function findById(id, notesArray) {
-    return notesArray.filter(note => note.id === id)[0];
-};
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './data/notes.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    )
-
-    return note;
-};
-
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string') {
-        return false;
-    }
-
-    return true;
-}
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.')
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    notes.splice(parseInt(req.params.id), 1);
-
-    let index = 0;
-    notes.forEach(note => {
-        note.id = `${index}`;
-        index++;
-    });
-    
-    fs.writeFileSync(
-        path.join(__dirname, './data/notes.json'),
-        JSON.stringify({ notes: notes }, null, 2)
-    );
-    res.sendStatus(200);
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
